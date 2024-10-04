94d59e9d19b7f3bb9647dc884db8608f22622feb
ANKUR-NARHARI-PANDE <157521628+ANKUR-NARHARI-PANDE@users.noreply.github.com>
2024-10-04 12:09:27 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -37,6 +37,27 @@ resource "aws_route_table_association" "private_route_association" {
   route_table_id = aws_route_table.private_route_table.id
 }
 
+resource "aws_security_group" "example" {
+  name        = "my_lambda_security_group"
+  description = "Security group for my Lambda function"
+  vpc_id      = aws_vpc.main.id  # Make sure to use the correct VPC ID
+
+  ingress {
+    from_port   = 80               # Allow incoming traffic on port 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]    # Allow traffic from all IPs (adjust as needed)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"             # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]    # Allow traffic to all IPs
+  }
+}
+
+
 resource "aws_lambda_function" "example" {
   function_name = "my_lambda_function"
   handler       = "lambda_function.lambda_handler"
