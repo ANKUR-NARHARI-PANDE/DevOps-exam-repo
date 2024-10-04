b529434cfd110e0ead6ee0bb83d3b340552b9768
ANKUR-NARHARI-PANDE <157521628+ANKUR-NARHARI-PANDE@users.noreply.github.com>
2024-10-04 13:43:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_route_table_association" "private_route_association" {
 
 
 resource "aws_lambda_function" "example_1" {
-  function_name = "my_lambda_function"
+  function_name = "my_lambda_function_1"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
   role          = data.aws_iam_role.lambda.arn
