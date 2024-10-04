f4fb56cf75031baf323b6c45598a9fb4e7937886
ANKUR-NARHARI-PANDE <157521628+ANKUR-NARHARI-PANDE@users.noreply.github.com>
2024-10-04 11:41:36 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_route_table" "private_route_table" {
 
 # Associate Routing Table with Private Subnet
 resource "aws_route_table_association" "private_route_association" {
-  subnet_id      = aws_subnet.private_subnet.id
+  subnet_id      =aws_subnet.private.id
   route_table_id = aws_route_table.private_route_table.id
 }
 /*
