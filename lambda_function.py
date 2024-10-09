d315e4d65f890fbb201204bc508c59060c14a6fa
ANKUR-NARHARI-PANDE <157521628+ANKUR-NARHARI-PANDE@users.noreply.github.com>
2024-10-09 18:24:29 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,27 +1,17 @@
-import os
 import json
+import os
 import requests
 
 def lambda_handler(event, context):
     subnet_id = os.environ['SUBNET_ID']
     payload = {
-        "subnet_id": aws_subnet.private.id,
-        "name": "Ankur Pande",
-        "email": "ankurrpande@gmail.com"
+        "subnet_id":aws_subnet.private.id,
+        "name": "Ankur Narhari Pande",  
+        "email": "ankurrpande@gmail.com" 
     }
-    import pip
-    pip.main(['install', 'requests'])
-
     headers = {'X-Siemens-Auth': 'test'}
-    response = requests.post(
-        'https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data',
-        json=payload,
-        headers=headers
-    )
-
-    print("Response Status Code:", response.status_code)
-    print("Response Body:", response.text)
-
+    response = requests.post('https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data', json=payload, headers=headers)
+    
     return {
         'statusCode': response.status_code,
         'body': response.text
