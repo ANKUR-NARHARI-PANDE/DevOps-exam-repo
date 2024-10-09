176d869f03df7d77fdb3aa593ea8775b363a8a75
ANKUR-NARHARI-PANDE <157521628+ANKUR-NARHARI-PANDE@users.noreply.github.com>
2024-10-09 18:46:43 +0530

    Add files via upload

--- lambda_function.py
+++ lambda_function.py
@@ -1,33 +1,56 @@
+# import os
+# import json
+# import requests
+
+# def lambda_handler(event, context):
+#     subnet_id = os.environ['SUBNET_ID']
+#     payload = {
+#         "subnet_id": aws_subnet.private.id,
+#         "name": "Ankur Pande",
+#         "email": "ankurrpande@gmail.com"
+#     }
+#     import pip
+#     pip.main(['install', 'requests'])
+
+#     headers = {'X-Siemens-Auth': 'test'}
+#     response = requests.post(
+#         'https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data',
+#         json=payload,
+#         headers=headers
+#     )
+
+#     print("Response Status Code:", response.status_code)
+#     print("Response Body:", response.text)
+
+#     return {
+#         'statusCode': response.status_code,
+#         'body': response.text
+#     }
+
+import boto3
 import json
 import os
-import requests
 
 def lambda_handler(event, context):
-    # Get the subnet ID from the environment variable
     subnet_id = os.environ['SUBNET_ID']
-    
-    # Create the payload
-    payload = {
-        "subnet_id": subnet_id,
-        "name": "Ankur Narhari Pande",  
-        "email": "ankurrpande@gmail.com" 
-    }
-    
-    # Set the headers
-    headers = {'X-Siemens-Auth': 'test'}
-    
-    # Send the POST request
-    try:
-        response = requests.post('https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data', json=payload, headers=headers)
-        response.raise_for_status()  # Raise an error for bad responses
-    except requests.exceptions.RequestException as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
-        }
-    
-    # Return the response from the API
+    name = "Ankur Pande"
+    email = "ankurrpande@gmail.com"
+
+    client = boto3.client('lambda')
+
+    response = client.invoke(
+        FunctionName="my_lambda_function_new",
+        Payload=json.dumps({
+            "subnet_id": subnet_id,
+            "name": name,
+            "email": email
+        })
+    )
+
+    payload = json.loads(response['Payload']['Payload'].read().decode('utf-8'))
+    print(payload)
+
     return {
-        'statusCode': response.status_code,
-        'body': response.text
+        'statusCode': 200,
+        'body': json.dumps(payload)
     }
